e004a44a21df24d5ea177cd29b0e1c97e7ca57f1
ChelseaWhite-Minger <35471927+ChelseaWhite-Minger@users.noreply.github.com>
2018-03-07 17:26:59 -0600

    Added while loop to check size !> 20

--- MultiplicationTables.java
+++ MultiplicationTables.java
@@ -15,13 +15,18 @@ public class MultiplicationTables {
         System.out.println("How large would you like them?");
         size = keyboard.nextInt();
 
+        while(size > 20 || size < 0) {
 
+            System.out.println("Please enter a value between 0 and 20");
+            size = keyboard.nextInt();
+
+        }
 
         for (int i = 0; i < size + 1; i++) {
 
             for (int j = 0; j < size + 1; j++) {
 
-                System.out.print(i + " * " + j + " = " + i*j + "\n");
+                System.out.print(i + " * " + j + " = " + i * j + "\n");
 
             }
         }
