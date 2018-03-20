f2533e7ddba6b585c144adca9faa61850059a9f3
ChelseaWhite-Minger <35471927+ChelseaWhite-Minger@users.noreply.github.com>
2018-03-19 21:25:30 -0500

    Add files via upload

---
+++ Temperatures.java
@@ -0,0 +1,101 @@
+package org.launchcode;
+
+import java.lang.Math;
+
+public class Temperatures {
+
+    public static void main(String[] args) {
+
+        String days[] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+        String times[] = {"7 AM", "3 PM", "7 PM", "3 AM"};
+        int sum3 = 0;
+        int avg3;
+
+        int temps[][] = new int[4][7];
+        temps[0][0] = 68;
+        temps[0][1] = 70;
+        temps[0][2] = 76;
+        temps[0][3] = 70;
+        temps[0][4] = 68;
+        temps[0][5] = 71;
+        temps[0][6] = 75;
+        temps[1][0] = 76;
+        temps[1][1] = 76;
+        temps[1][2] = 87;
+        temps[1][3] = 84;
+        temps[1][4] = 82;
+        temps[1][5] = 75;
+        temps[1][6] = 83;
+        temps[2][0] = 73;
+        temps[2][1] = 72;
+        temps[2][2] = 81;
+        temps[2][3] = 78;
+        temps[2][4] = 76;
+        temps[2][5] = 73;
+        temps[2][6] = 77;
+        temps[3][0] = 64;
+        temps[3][1] = 65;
+        temps[3][2] = 69;
+        temps[3][3] = 68;
+        temps[3][4] = 70;
+        temps[3][5] = 74;
+        temps[3][6] = 72;
+
+        System.out.println("Temperature Calculator\n");
+        System.out.println("The data provided are:");
+        for (int row = 0; row < 4; row++) {
+            if (row > 0) {
+                System.out.println();
+            }
+
+            System.out.print(times[row] + ": ");
+
+            for (int column = 0; column < 7; column++) {
+                System.out.print(temps[row][column]);
+                if (column < 6) {
+                    System.out.print(", ");
+                }
+            }
+        }
+
+        System.out.println("\n\nBased on that data, the following are the average temperatures for the week.\n");
+
+        for (int day = 0; day < 7; day++) {
+            int sum1 = 0;
+            int avg1 = 0;
+            System.out.print(days[day] + ": ");
+
+            for (int row = 0; row < 4; row++) {
+                sum1 += temps[row][day];
+            }
+            avg1 = Math.round(sum1/4);
+            System.out.println(avg1);
+
+        }
+
+        System.out.print("\n\n");
+
+        for (int time = 0; time < 4; time++) {
+            int sum2 = 0;
+            int avg2 = 0;
+            System.out.print(times[time] + ": ");
+
+            for (int column = 0; column < 7; column++){
+                sum2 += temps[time][column];
+            }
+            avg2 = Math.round(sum2/7);
+            System.out.println(avg2);
+        }
+
+        System.out.print("\n\nThe final average temperature for the week was:\n\nOverall: ");
+
+        for (int row = 0; row < 4; row++){
+            for (int column = 0; column < 7; column++){
+                sum3 += temps[row][column];
+            }
+        }
+        avg3 = sum3/28;
+        System.out.print(avg3);
+    }
+}
+
